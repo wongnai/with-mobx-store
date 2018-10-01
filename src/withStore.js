00a8ea11234f8c4fe413a07595382d57b3dfd276
voraton.l <voraton@wongnai.com>
2018-10-01 10:42:21 +0700

    Add onUnMounted and onMounted lifecycle

--- src/withStore.js
+++ src/withStore.js
@@ -1,25 +1,39 @@
 import { Provider } from 'mobx-react'
-import React from 'react'
+import React, { PureComponent } from 'react'
 
 /**
  * withStore is a higher order component (HOC) that allow you to initialize component without wraping any provider
  * @param { { string: Store } } stores                  - Object for mapping name of store with store object. 
- * @param {Object}              options                 - Options for modifying store lifecycle
- * @param {function}      options.onInitialized   - Function which is executed when the initialization of stores is complete.
+ * @param {Object}              options                 - Options for modifying store lifecycle.
+ * @param {function}            options.onInitialized   - Function which is executed when the initialization of stores is complete.
+ * @param {function}            options.onMounted       - Function which is executed when the component is mounted.
+ * @param {function}            options.onUnMounted     - Function which is executed when the component is unmounted.
  */
 const withStore = (stores, options) => {
     if (options) {
-        const { onInitialized } = options
+        const { onInitialized, onMounted } = options
         if (onInitialized) {
             onInitialized(stores)
         }
     }
     return Component => {
-        const ComponentWithStore = props => (
-            <Provider {...stores}>
-                <Component {...props} />
-            </Provider>
-        )
+        class ComponentWithStore extends PureComponent {
+            componentDidMount() { 
+                onMounted(stores, this.props)
+            }
+
+            componentWillUnmount() {
+                onUnMounted(stores, this.props)
+            }
+
+            render() {
+                return (
+                    <Provider {...stores}>
+                        <Component {...this.props} />
+                    </Provider>
+                )
+            }
+        }
         ComponentWithStore.displayName = 'withStore'
         return ComponentWithStore
     }
