91e6765ff34af06b55e3137a8cfb18fd2b926614
voraton.l <voraton@wongnai.com>
2019-07-08 23:45:36 +0700

    Initialize Store inside Component

--- src/withComponentStore.js
+++ src/withComponentStore.js
@@ -1,4 +1,6 @@
 import withStore from './withStore'
+import React, { useMemo, useEffect } from 'react'
+import { Provider } from 'mobx-react'
 
 /**
  * withComponentStore is higher order component (HOC) that allow you to initialize component without wraping any provider.
@@ -8,11 +10,28 @@ import withStore from './withStore'
  * @param {async function}      options.onInitialized   - Function which is executed when the initialization of stores is complete.
  */
 const withComponentStore = (stores, options) => Component => {
-    const initializedStores = {}
-    for (const storeName in stores) {
-        initializedStores[storeName] = new stores[storeName]()
+    const { onInitialized, onMounted, onUnMounted } = options
+    const ComponentWithComponentStore = props => {
+        const stores = useMemo(() => {
+            const initializedStores = {}
+            for (const storeName in stores) {
+                initializedStores[storeName] = new stores[storeName]()
+            }
+            onInitialized(initializedStores)
+            return initializedStores
+        }, [])
+        useEffect(() => {
+            onMounted && onMounted(stores, props)
+            return () => {
+                onUnMounted && onUnMounted(stores, props)
+            }
+        }, [])
+        return (
+            <Provider>
+                <Component {...props} />
+            </Provider>
+        )
     }
-    const ComponentWithComponentStore = withStore(initializedStores, options)(Component)
     ComponentWithComponentStore.displayName = 'withComponentStore'
     return ComponentWithComponentStore
 }
